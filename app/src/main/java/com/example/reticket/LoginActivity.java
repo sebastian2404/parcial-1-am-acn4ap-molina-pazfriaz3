42acf20d161d40ddc1fa219f4ac7a0c21fedc585
sebastian2404 <148718385+sebastian2404@users.noreply.github.com>
2023-12-01 18:08:50 -0300

    El usuario ahora podra Registrarse y si los datos estan bien, se lo enviara a la home

--- app/src/main/java/com/example/reticket/LoginActivity.java
+++ app/src/main/java/com/example/reticket/LoginActivity.java
@@ -41,6 +41,27 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
+    public void CreateUser(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(),Login.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +80,21 @@ public class LoginActivity extends AppCompatActivity {
         Log.i("Login Password", passwordString);
 
         this.login(emailString,passwordString);
+
+
+    }
+
+    public void Registrarse(View v){
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        String emailString = email.getText().toString();
+        String passwordString  = password.getText().toString();
+
+        Log.i("Login email", emailString);
+        Log.i("Login Password", passwordString);
+
+        this.CreateUser(emailString,passwordString);
 
 
     }
