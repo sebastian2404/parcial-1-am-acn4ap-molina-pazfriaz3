650176bd801de5ff592042fb854f0f57453576c4
sebastian2404 <148718385+sebastian2404@users.noreply.github.com>
2023-12-01 15:05:43 -0300

    Creacion de un Login y SignOut (falta terminar)

---
+++ app/src/main/java/com/example/reticket/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.example.reticket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    public void login(String email,  String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onLogin(View v){
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        String emailString = email.getText().toString();
+        String passwordString  = password.getText().toString();
+
+        Log.i("Login email", emailString);
+        Log.i("Login Password", passwordString);
+
+        this.login(emailString,passwordString);
+
+
+    }
+}
